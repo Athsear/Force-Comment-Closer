c81965f9d14774d1c01368d0e74c845d5cbe6633
athsear <athsear@gmail.com>
2014-11-16 23:00:02 +0900

    晩御飯食べてたら「ちょっとまて」ってなったので箇所全部なおし

--- force_comment_closer/force_comment_closer.js
+++ force_comment_closer/force_comment_closer.js
@@ -3,16 +3,17 @@
 		$("<div>")
 		.addClass("force_comment_closer_block alignleft actions")
 		.append($("<form>")
-			.attr({name:"force_comment_closer",method:"POST"})
+			.addClass("postform force_comment_closer_form")
+			.attr({name:"force_comment_closer_form",method:"POST"})
 			.append($("<select>")
 				.append($('<option>').val("open").text("全投稿でコメントを許可"))
 				.append($('<option>').val("close").text("全投稿でコメントを不許可"))
-				.addClass("postform")
 				.attr("name","force_comment_closer")
 			)
 			.append(
 				$("<button>")
 				.addClass("button")
+				.attr("type","button")
 				.text("全投稿一括コメント許可設定変更")
 				.on("click",function(){
 					var mode = "許可";
@@ -20,7 +21,9 @@
 						mode = "不許可";
 					}
 					if(window.confirm("全ての投稿のコメントを" + mode + "に設定します。\nよろしいですか？")){
-						submit();
+						$("form.force_comment_closer_form").submit();
+					} else {
+						return true;
 					}
 				})
 			)
