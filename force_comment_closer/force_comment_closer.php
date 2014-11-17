d5d256c1ceea3b1b0afd0c8836dbcffe8b80171f
athsear <athsear@gmail.com>
2014-11-18 00:52:37 +0900

    更新時にリファラを見るようにしました。

--- force_comment_closer/force_comment_closer.php
+++ force_comment_closer/force_comment_closer.php
@@ -12,18 +12,26 @@
 $rotater = new force_comment_closer();
 
 class force_comment_closer {
+	const TARGET_PAGE = "edit.php";
+	const PARAM_OPEN = "open";
+	const PARAM_CLOSE = "close";
+	
 	function __construct() {
 		GLOBAL $pagenow;
-		if (is_admin() and $pagenow == "edit.php") {
+		if (is_admin() and $pagenow == self::TARGET_PAGE) {
 			add_action("admin_enqueue_scripts",array($this,"set_enqueue"));
 			$input = filter_input(INPUT_POST,"force_comment_closer");
-			if($input == "open" or $input == "close") {
+			$checkUrl = admin_url() . self::TARGET_PAGE;
+			
+			if(strpos(filter_input(INPUT_SERVER,"HTTP_REFERER"),$checkUrl) === 0
+				and ($input === self::PARAM_OPEN or $input === self::PARAM_CLOSE)){
+
 				GLOBAL $wpdb;
 				$sql = "UPDATE ".$wpdb->posts." SET comment_status = %s";
-				if(filter_input(INPUT_POST,"force_comment_closer") == "close") {
-					$wpdb->query($wpdb->prepare($sql,"close"));
-				} else if(filter_input(INPUT_POST,"force_comment_closer") == "open"){
-					$wpdb->query($wpdb->prepare($sql,"open"));
+				if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_CLOSE) {
+					$wpdb->query($wpdb->prepare($sql,self::PARAM_CLOSE));
+				} else if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_OPEN){
+					$wpdb->query($wpdb->prepare($sql,self::PARAM_OPEN));
 				}
 			}
 		}
