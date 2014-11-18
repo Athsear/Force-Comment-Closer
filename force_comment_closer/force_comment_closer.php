771e0f14dffe32ea637f120a970b4942b9aa044a
athsear <athsear@gmail.com>
2014-11-19 01:18:20 +0900

    post時はcheck_admin_referer()でチェックしてみるように（ただ、debugをtrueにしてるとnonceも使ってってめっちゃWordPressから注意されます

--- force_comment_closer/force_comment_closer.php
+++ force_comment_closer/force_comment_closer.php
@@ -20,24 +20,26 @@ class force_comment_closer {
 		GLOBAL $pagenow;
 		if (is_admin() and $pagenow == self::TARGET_PAGE) {
 			add_action("admin_enqueue_scripts",array($this,"set_enqueue"));
-			$input = filter_input(INPUT_POST,"force_comment_closer");
-			$checkUrl = admin_url() . self::TARGET_PAGE;
-			
-			if(strpos(filter_input(INPUT_SERVER,"HTTP_REFERER"),$checkUrl) === 0
-				and ($input === self::PARAM_OPEN or $input === self::PARAM_CLOSE)){
+			add_action('admin_menu',array($this,'updateCommentStatus'));
 
-				GLOBAL $wpdb;
-				$sql = "UPDATE ".$wpdb->posts." SET comment_status = %s";
-				if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_CLOSE) {
-					$wpdb->query($wpdb->prepare($sql,self::PARAM_CLOSE));
-				} else if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_OPEN){
-					$wpdb->query($wpdb->prepare($sql,self::PARAM_OPEN));
-				}
-			}
 		}
 	}
 	
 	function set_enqueue() {
 		wp_enqueue_script("force_comment_closer",plugin_dir_url( __FILE__ ) ."force_comment_closer.js",array(),"1.0",true);
+	}
+	
+	function updateCommentStatus() {
+		$input = filter_input(INPUT_POST,"force_comment_closer");
+		if($input === self::PARAM_OPEN or $input === self::PARAM_CLOSE){
+			check_admin_referer();
+			GLOBAL $wpdb;
+			$sql = "UPDATE ".$wpdb->posts." SET comment_status = %s";
+			if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_CLOSE) {
+				$wpdb->query($wpdb->prepare($sql,self::PARAM_CLOSE));
+			} else if(filter_input(INPUT_POST,"force_comment_closer") === self::PARAM_OPEN){
+				$wpdb->query($wpdb->prepare($sql,self::PARAM_OPEN));
+			}
+		}
 	}
 }
